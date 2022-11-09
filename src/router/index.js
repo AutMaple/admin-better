fbd761940bb465fd0afb4f6a6ca84bb2576c52fd
AutMaple <838498279@qq.com>
2022-11-09 17:05:06 +0800

    实现 404 页面

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ export default new VueRouter({
     {
       path: "/401",
       component: () => import("@/views/401")
+    },
+    {
+      path: "/404",
+      component: () => import("@/views/404")
     }
+
   ],
 });
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 import user from "./modules/user"
 import settings from "./modules/settings"
+import tabsBar from "./modules/tabsBar"
 
 Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     user,
-    settings
+    settings,
+    tabsBar
   }
 });
